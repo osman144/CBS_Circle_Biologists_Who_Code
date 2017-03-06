1eead8471f3380ec93a4f6103b7f07c7360ffdec
Dimitri Sidiropoulos <sidir003@umn.edu>
2017-03-06 01:28:58 -0600

    null

--- allFunctions.py
+++ allFunctions.py
@@ -1,4 +1,7 @@
-#class proteinSeq tbc
+class proteinSeq:
+    def __init__(self, protein):
+        self.__protein = protein
+
 #class ntSeq tbc
 
 pka = {'A': 'null', 'F':'null', 'G':'null', 'S':'null', 'I':'null', 'L':'null', 'M':'null', 'N':'null', 'P':'null', 'Q':'null', 'T':'null','W':'null', 'V':'null','H':6.0, 'D':3.7, 'E':4.2, 'Y':10.5, 'C':8.2, 'K':10.5, 'R':12.5}
